42390e8cc544c972460efb67da76ce761731b4fd
DTA32 <97021335+DTA32@users.noreply.github.com>
2025-05-28 19:01:01 +0700

    Revert "fix kproto pca"
    This reverts commit 9f4cc97826801503cf16a2d9cc926114ed991f8a.

--- 15-final/KPROTO.py
+++ 15-final/KPROTO.py
@@ -1,16 +1,14 @@
 from kmodes.kprototypes import KPrototypes
-
-import utils
 from mother import Mother
 
 class KProto(Mother):
     def __init__(self):
         super().__init__(name="K-Prototype", polling_interval=1, is_rfmd=True)
-        # using RFMD, customized here
-        self.kproto_df = self.RFM_numerical.copy()
-        self.kproto_df['State'] = self.RFM_categorical
     
     def _run_clustering(self):
+        # using RFMD, need to be customized here
+        kproto_df = self.RFM_numerical.copy()
+        kproto_df['State'] = self.RFM_categorical
         # algo
         kproto = KPrototypes(
             n_clusters=4, 
@@ -20,13 +18,10 @@ class KProto(Mother):
             n_jobs=-1
         )
         labels = kproto.fit_predict(
-            self.kproto_df, 
+            kproto_df, 
             categorical=[3]
         )
         return labels
-    
-    def _run_eval_metrics(self, labels):
-        utils.eval_metrics_single(self.kproto_df, labels, self.logger)
 
 if __name__ == "__main__":
     kproto_impl = KProto()
